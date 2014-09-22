6fba2fe4867a6f93d3a0e8d078452cea25581e05
derui <derutakayu@gmail.com>
2014-09-22 18:27:50 +0900

    [from now] 2014/09/22 18:27:50
    diff --git a/src/OMakefile b/src/OMakefile
    index 4ee3c8f..285e2cc 100644
    --- a/src/OMakefile
    +++ b/src/OMakefile
    @@ -44,9 +44,7 @@ open MyUtil
     USE_OCAMLFIND = true
     #
     OCAMLPACKS[] =
    -	camlp4.extend
    -	camlp4.quotations.o
    -	camlp4.lib
    +	compiler-libs.common
     #
     if $(not $(OCAMLFIND_EXISTS))
        eprintln(This project requires ocamlfind, but is was not found.)
    @@ -58,7 +56,7 @@ if $(not $(OCAMLFIND_EXISTS))
     #
     OCAMLINCLUDES +=
    
    -OCAMLFINDFLAGS = -syntax camlp4o
    +OCAMLFINDFLAGS =
    
     #
     # Compile native or byte code?
    @@ -88,18 +86,18 @@ FILES[] =
     	specFormat
     #
     PACKAGE = simplespec
    -LIB = pa_spec
    +LIB = ppx_spec
     #
    -# pa_spec以外をパッケージにする
    +# ppx_spec以外をパッケージにする
     MyOCamlPackage($(PACKAGE), $(FILES), $(EMPTY), -linkall, false)
    -MyOCamlLibrary($(LIB), pa_spec)
    +OCamlProgram($(LIB), ppx_spec)
    
    -lib: $(OCamlPackageGeneratedFiles $(PACKAGE), false) $(OCamlLibrary $(LIB), pa_spec)
    +lib: $(OCamlPackageGeneratedFiles $(PACKAGE), false) $(OCamlLibrary $(LIB), ppx_spec)
    
     INSTALL_FILES[] =
     INSTALL_FILES += $(OCamlPackageGeneratedFiles $(PACKAGE), false)
    -INSTALL_FILES += $(OCamlLibrary $(LIB), pa_spec)
    +INSTALL_FILES += $(OCamlProgram $(LIB), ppx_spec)
    
     OCamlInstallPackage($(PACKAGE), $(INSTALL_FILES))
    
    -.DEFAULT: lib
    +.DEFAULT: lib $(OCamlProgram $(LIB), ppx_spec)
    diff --git a/src/tmp.ml b/src/tmp.ml
    index f81e0b9..475ceb6 100644
    --- a/src/tmp.ml
    +++ b/src/tmp.ml
    @@ -1,18 +1,14 @@
    -let spec = Spec.new_spec "hoge"
    -in
    -  (spec,
    -   (fun () ->
    -      (Spec.start spec;
    -       (let example = Spec.new_example "test"
    -        in
    -          (Spec.add_example example;
    -           Spec.start_example example;
    -           (try
    -              if "hoge" = "huga"
    -              then Spec.add_successful_expectation ()
    -              else Spec.add_failure_expectation "( = )" "hoge" "huga"
    -            with | e -> Spec.add_error (Printexc.to_string e));
    -           Spec.end_example example));
    -       Spec.remove_spec spec)))
    -
    
    +[%describe
    +[%it [@attr]
    +let x = "test" in
    +let y = "foobar" in
    +Printf.printf "%s %s" x y
    +]
    +;
    +[%it
    +let x = "test" in
    +let y = "foobar" in
    +Printf.printf "%s %s" x y
    +]
    +]
    \ No newline at end of file

--- src/tmp.ml
+++ src/tmp.ml
@@ -1,18 +1,14 @@
-let spec = Spec.new_spec "hoge"
-in
-  (spec,
-   (fun () ->
-      (Spec.start spec;
-       (let example = Spec.new_example "test"
-        in
-          (Spec.add_example example;
-           Spec.start_example example;
-           (try
-              if "hoge" = "huga"
-              then Spec.add_successful_expectation ()
-              else Spec.add_failure_expectation "( = )" "hoge" "huga"
-            with | e -> Spec.add_error (Printexc.to_string e));
-           Spec.end_example example));
-       Spec.remove_spec spec)))
 
-
+[%describe
+[%it [@attr]
+let x = "test" in
+let y = "foobar" in
+Printf.printf "%s %s" x y 
+]
+;
+[%it
+let x = "test" in
+let y = "foobar" in
+Printf.printf "%s %s" x y 
+]
+]
